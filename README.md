19016788cc981e39bffd336dda211362b0355abc
Testa <testa@airndsoft.com>
2014-11-08 22:42:16 +0000

    first yes

---
+++ README.md
@@ -0,0 +1,9 @@
+Z.I.T.E.
+
+Make JavaScript shorter. 
+
+Thats nothing :P Just I am not the fastest typer. 
+
+Check the code it's easy.
+
+Lasy developers only.
---
+++ zite.js
@@ -0,0 +1,46 @@
+
+function c(s){
+    return document.getElementsByClassName(s)[0];
+}
+function cs(s){
+    return document.getElementsByClassName(s)[0].style;
+}
+
+function cv(s){
+    return document.getElementsByClassName(s)[0].value;
+}
+
+function cw(s, v){
+    return document.getElementsByClassName(s)[0].value=v;
+}
+
+
+function d(s){
+    return document.getElementById(s);
+}
+function ds(s){
+    return document.getElementById(s).style;
+}
+
+function dv(s){
+    return document.getElementById(s).value;
+}
+
+function dw(s, v){
+    return document.getElementById(s).value = v;
+}
+
+
+function n(s) {
+    return document.getElementsByName(s)[0];
+}
+function ns(s) {
+    return document.getElementsByName(s)[0].style;
+}
+
+function nv(s) {
+    return document.getElementsByNamen(s)[0].value;
+}
+function nw(s, v) {
+    document.getElementsByNamen(s)[0].value=v;
+}
